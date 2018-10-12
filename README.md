08c946322c179c919f022509a5b9aacdcb483b2d
RaihanKabir <iraihankabir@yahoo.com>
2018-10-12 13:20:16 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
-# LOGIC_RECOGNIZER
+# LOGIC RECOGNIZER
+> Goal of this project is to recognize logics behind the Logic-gates 
+> of electronics, using DeepLearning Method. But you are not constrained
+> to that. You can modify some pieces of code to make it useful to recognize 
+> any logic :-)
+> The recognizer was tested against **AND, NAND, NOR, EXNOR, EXNAND**
+> logic-gates and the output is quite promising. It gives 100% accuracy
+---
+
+## LOGIC-GATE TRUTH TABLES
+![image](/img/truth_table.gif)
+
+### Schematic representation of 2-3-2 Network (Net)
+
+![image](/img/2-3-2net.png)
+
+### 2-3-2 Net with weights, biases and activations.
+
+![image](/img/2-3-2detailnet.jpg)
+
+> **NB:** You can modify the network structure as like you wish. e.g. 2-3-3-2.
+---
+
+### Sigmoid function
+> Reduce any real number value in between 0 to 1.0
+![image](/img/sigmoid.png)
+
+
+---
+# Introduction
+We want to train our recognizer/classifer with some data, showing the output and and we want 
+the network to test with any input data. We don't want to specify the logic manually instead we expect the network to understand/learn the logics itself by analyzing the input/label data. Although it's not neccessary to implement DeepLearning method to this simple if/else
+job.
+
+# STRUCTURE
+* **Dataset:** 
+	* Generates random data and label.
+	* 2 input data i.e. x0 and x1
+	* Split data and label to test and train portion
+
+* **Network:**
+	* Initialize random weights and biases
+	* Forward propagation: activations calculation
+	* Loss/Cost calculation: measurement of network's behaviour
+	* Delta values (for weights & biases) calculation with **Gradient descent** method
+	* Backward propagation: modification of connection strengths & biases by updating them
+	* Testing the network's performance
+
+> Output layer got 2 neurons, 1st neuron's activation means output is 0 and 2nd's means output is 1.
+---
+
+# STEPS
+* Calculate layer by layer activations
+* Calculate loss/cost function
+* Calculate cost/loss derivative
+* Calculate sigmoid derivative/derivative of activation
+* Calculate delta and nabla values for weights and biases
+* Define learning rate
+* Update weights and biases
+
+## Activation calculation
+```python
+	a = self.sigmoid(np.dot(w,a) + b)
+```
+## Loss/Cost calculation
+```python
+	'''a[-1] is the activations of output layer and 
+	n is no. of training data'''
+	self.loss = np.sum(np.power(np.subtract(self.a[-1], label), 2)) / 2*self.dataset.batch_size
+```
+### Cost derivative
+```python
+	cost_derivative = np.subtract(self.a[-1], y)
+```
+### Derivative of activation
+```python
+	def sigmoid_derivative(a):
+		return a * (1-a)
+```
+
+### Delta and nabla calculations
+**For output layer**
+```python
+	delta = cost_derivative * self.sigmoid_derivative(self.a[-1])
+	self.nabla_w[-1] = delta.dot(self.a[-2].T)
+	self.nabla_b[-1] = delta
+```
+**For hidden layers:** codes for the previous layer of output
+```python
+	# delta in the right hand side is for output layer
+	delta = np.dot(self.weights[-1].T, delta) * self.sigmoid_derivative(self.a[-2])
+	self.nabla_b[-i] = delta
+	self.nabla_w[-i] = np.dot(delta, self.a[-3].T)
+```
+
+### Update weights and biases
+```python
+	weight = weight - (learning_rate/self.dataset.batch_size)*nabla_w
+	bias = bias - (learning_rate/self.dataset.batch_size)*nabla_b
+```
+
+
+### Execute the codes
+
+**Install dependencies**
+```bash
+	pip3 install -r requirements.txt
+```
+**Codes were written for Python v3.6.6**
+
+> Primarily I've set the network as [2-3-2] i.e. 1 hidden layer having 3 neurons and output
+> layer having 2 neurons. You can change it in `n = Network([2,3,2]size=10000)`. 
+> Off course you don't want to change first and last layer's neuron numbers. For 
+> example You can have 2 hidden layer then you'd provide something like
+> `n = Network([2,3,3,2]size=10000)`
+
+
+
+
+**Run a logic gate**
+```bash
+	python3 andGate.py
+```
---
+++ norGate.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+import numpy as np
+import random
+
+class Dataset():
+	def __init__(self, size=10, tt_ratio=0.2, batch_size=1):
+		self.size = size
+		self.tt_ratio = tt_ratio
+		self.testNo = int(size*tt_ratio)
+		self.current_indx = 0
+		self.current_epoch = 0
+		self.batch_size = batch_size
+		self.total_batch_per_epoch = size - self.testNo
+		self.set_datasets()
+
+	def set_datasets(self):
+		self.generate_data()
+		self.test_d = self.data[0:self.testNo]
+		self.test_l = self.label[0:self.testNo]
+		self.train_d = self.data[self.testNo:]
+		self.train_l = self.label[self.testNo:]
+
+	@staticmethod
+	def getLabel(x1, x2):
+		''' NAND GATE LOGIC HERE '''
+		arr = np.array([0,0])
+		if x1 == x2 and x1 == 0:
+			arr[1] = 1
+			return arr.T
+		arr[0] = 1
+		return arr.T
+
+	def generate_data(self):
+		self.data = np.array(np.random.random([self.size, 2]) >= 0.5, dtype=np.int32)
+		self.label= np.array([ self.getLabel(d[0],d[1]) for d in self.data])
+
+	@property
+	def get_next(self):
+		if self.current_indx+1 >= self.total_batch_per_epoch:
+			self.train_d, self.train_l = self.shuffle_data(
+								self.train_d, 
+								self.train_l
+							)
+			self.current_indx = 0
+			self.current_epoch += 1
+		batch_input = self.train_d[self.current_indx:(self.current_indx+self.batch_size)]
+		batch_label = self.train_l[self.current_indx:(self.current_indx+self.batch_size)]
+		self.current_indx += 1
+		return [batch_input, batch_label]
+
+	def shuffle_data(self, data, label):
+		combind_data = list(zip(data, label))
+		random.shuffle(combind_data)
+		_x, _y = list(zip(*combind_data))
+		_x = np.array(_x)
+		_y = np.array(_y)
+		return [_x, _y]
+
+class Network():
+	def __init__(self, layers=[2,3,2], size=10):
+		'''
+			in the argument layers, at index 0, 2 is input size
+			so, this is not related to any weights and biases
+		'''
+		self.biases  = [np.random.randn(y, 1) for y in layers[1:]]
+		self.weights = [np.random.randn(y, x)
+						for x, y in zip(layers[:-1], layers[1:])]
+		self.nabla_b = [np.zeros(b.shape) for b in self.biases]
+		self.nabla_w = [np.zeros(w.shape) for w in self.weights]
+		self.a = None
+		self.z = None
+		self.layersNo = len(layers)
+		self.layers = layers
+		self.dataset = Dataset(size=size, tt_ratio=0.2)
+		self.loss = None
+		self.losses = []
+
+	@staticmethod
+	def sigmoid(z):
+		return 1 / (1+np.exp(-z))
+
+	@staticmethod
+	def sigmoid_derivative(a):
+		return a * (1-a)
+
+	def feedforward(self, a):
+		'''
+			For backpropagation the input data (i.e. a0, 
+			input neurons activations) are needed
+			to update weights and biases of the first layer
+		'''
+		A,Z = [a], [None]		
+		for w, b in zip(self.weights, self.biases):
+			Z.append(np.dot(w,a) + b)
+			a = self.sigmoid(np.dot(w,a) + b)
+			A.append(a)
+		self.a = A
+		self.z = Z
+
+	def calculate_loss(self, label):
+		self.loss = np.sum(np.power(np.subtract(self.a[-1], label), 2))
+		self.losses.append(self.loss)
+
+	def calculate_deltas(self, y):
+		'''
+			The calculation of the output layer deltas are
+			different than the hidden layer's. So We'll deal 
+			with the delta calculations considering that fact.
+
+			delta^L = a^L-1 * der(a^L) * (a^L-y); 			   # for output neurons
+			delta^L-1 = a^L-2 * der(a^L-1) * (w^L-1 * delta^L) #for hidden
+															   #layer neurons
+		'''
+		cost_derivative = np.subtract(self.a[-1], y)
+		delta = cost_derivative * self.sigmoid_derivative(self.a[-1])
+		self.nabla_w[-1] = delta.dot(self.a[-2].T)
+		self.nabla_b[-1] = delta
+		# nabla for hidden layers
+		for i in range(2, self.layersNo):
+			delta = np.dot(self.weights[-i+1].T, delta) * self.sigmoid_derivative(self.a[-i])
+			self.nabla_b[-i] = delta
+			self.nabla_w[-i] = np.dot(delta, self.a[-i-1].T)
+
+	def update_weights_biases(self, learning_rate):
+		'''
+			new_weight = weight + (-1 * learningRate * nabla_w)
+			new_bias = bias + (-1 * learningRate * nabla_b)
+		'''
+		self.weights = [w-(learning_rate/self.dataset.batch_size)*nw 
+						for w, nw in zip(self.weights, self.nabla_w)]
+		self.biases = [b-(learning_rate/self.dataset.batch_size)*nb 
+						for b, nb in zip(self.biases, self.nabla_b)]
+
+	def backwardprop(self, label, learning_rate=0.01):
+		self.calculate_loss(label)
+		self.calculate_deltas(label)
+		self.update_weights_biases(learning_rate)
+
+	def predict(self, a):
+		for w, b in zip(self.weights, self.biases):
+			a = self.sigmoid(np.dot(w,a) + b)
+		return a
+
+	def train(self, iterationNo=1000):
+		for i in range(iterationNo):
+			data, label = self.dataset.get_next
+			self.feedforward(data.T)
+			self.backwardprop(label.T, learning_rate=0.01)
+			print("Training %s: Loss = %s"%(
+					i, self.loss
+				)
+			)
+
+	@staticmethod
+	def extract_output(a):
+		return a.argmax()
+
+	def test(self):
+		correct = 0
+		index = 1
+		for data, label in zip(self.dataset.test_d, self.dataset.test_l):
+			data = np.array(np.matrix(data).T)
+			label = np.array(np.matrix(label).T)
+			prediction = self.extract_output(self.predict(data))
+			y = self.extract_output(label)
+			if prediction == y:
+				correct += 1
+			print("Test %s: Expected: %s, Predicted: %s, Accuracy: %s"%(
+					index, y, prediction, round((correct/index), 2)
+				)
+			)
+			index += 1
+
+n = Network([2,3,2], size=1000)
+n.train(iterationNo=100000)
+n.test()
